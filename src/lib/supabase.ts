ea46300bc913d575ab2e8693e028c854e74c3bbe
gaston10com <enzothome1@gmail.com>
2026-01-17 15:49:24 -0300

    chore

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,9 +3,13 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
+console.log('ENV', import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY)
+
+
 if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Missing Supabase environment variables.')
+  console.warn('Missing Supabase env vars. Check .env.local')
 }
+
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
